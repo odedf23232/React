6ca856d2e11d0a1aab83ce25dd966db17648ffcc
Oded Fisher <odedf2323@gmail.com>
2023-04-09 23:29:13 +0300

    Add files via upload

---
+++ ListGroup.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+}
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
