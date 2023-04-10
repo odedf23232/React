3485a64e464757fe5249618fb07422f436385b2e
Oded Fisher <odedf2323@gmail.com>
2023-04-10 21:26:43 +0300

    Create Alert.tsx

---
+++ Alert.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return (
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
