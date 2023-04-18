034bf3937ac940a8ef80038bbdbf01c7c314c336
Oded Fisher <odedf2323@gmail.com>
2023-04-18 17:11:27 +0300

    Create Form.tsx

---
+++ Form.tsx
@@ -0,0 +1,40 @@
+import React, { FormEvent, useState } from "react";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
+
+const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
